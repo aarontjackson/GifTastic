95da4f4516ca6cb4cd76ab05f5663cc4d8aa8440
Aaron Jackson <aarontjackson@outlook.com>
2019-09-14 17:08:46 -0400

    addition of gipy api url

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -5,8 +5,18 @@ let newPlaces = [];
 
 console.log(topics);
 
+// GIPHY URL
+
+//javascript, jQuery GIPHY URL, which will include a user input search & 10 gif limit
+
+var xhr = $.get(`"http://api.giphy.com/v1/gifs/search?q=${}&api_key=nf5xbhF78wvJVnNt6j0vQ9XrNgNlkPL9&limit=10"`);
+xhr.done(function(data) { console.log("success got data", data); });
+
+ let apiKey = nf5xbhF78wvJVnNt6j0vQ9XrNgNlkPL9;
+
 // Take the topics array, and create an element to hold new buttons
 
+const placeBtn = $("<button>");
 
 
 // File through the topics array and display buttons on the window
