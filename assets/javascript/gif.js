586e33ce204839f1c97bd1ccbad51ac1cce2dd45
Aaron Jackson <aarontjackson@outlook.com>
2019-09-14 16:09:38 -0400

    Added steps to take to achieve project goal. Created a topics array and console.log to see if it printed successfully

---
+++ assets/javascript/gif.js
@@ -0,0 +1,24 @@
+// Create an array of strings
+
+let topics = ["Orlando", "Florida", "Paris", "France", "Maryland"];
+let newPlaces = [];
+
+console.log(topics);
+
+// Take the topics array, and create an element to hold new buttons
+
+
+
+// File through the topics array and display buttons on the window
+
+// Click on the buttons to display 10 Gif images w/o movement
+
+// Click on the Gif images to cause movement
+
+// Display Gif rating above the images
+
+// User input takes value and adds to topic array
+
+// Function that takes in topic array 
+
+// Function remakes buttons on page with User input
