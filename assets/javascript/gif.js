eec084692066a4bcdc0b4bf95cd3d0f78e8f0929
Aaron Jackson <aarontjackson@outlook.com>
2019-09-14 20:13:40 -0400

    created functions to display buttons on the window of the items in the topic array

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,23 +1,54 @@
 // Create an array of strings
-
 let topics = ["Orlando", "Florida", "Paris", "France", "Maryland"];
-let newPlaces = [];
 
-console.log(topics);
+console.log("It's working");
 
 // GIPHY URL
 
 //javascript, jQuery GIPHY URL, which will include a user input search & 10 gif limit
+// var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=nf5xbhF78wvJVnNt6j0vQ9XrNgNlkPL9";
 
-var xhr = $.get(`"http://api.giphy.com/v1/gifs/search?q=${}&api_key=nf5xbhF78wvJVnNt6j0vQ9XrNgNlkPL9&limit=10"`);
-xhr.done(function(data) { console.log("success got data", data); });
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log("success got data", response);
 
- let apiKey = nf5xbhF78wvJVnNt6j0vQ9XrNgNlkPL9;
+// });
 
+
+
+function renderButtons() {
+    $("#gifBtn").empty();
+
+    for (let i = 0; i < topics.length; i++) {
+        console.log("looping was a success");
+
+        let btn = $("<button>");
+        btn.attr({
+            "class": 'gif',
+            "data-name": topics[i]
+        });
+        btn.text(topics[i]);
+        $("#gifBtn").append(btn);
+    }
+}
 // Take the topics array, and create an element to hold new buttons
+$("#addGif").on("click", function(event) {
+    // event.preventDefault() prevents the form from trying to submit itself.
+    // We're using a form so that the user can hit enter instead of clicking the button if they want
+    event.preventDefault();
 
-const placeBtn = $("<button>");
+    // This line will grab the text from the input box
+    let gif = $("#gifInput").val().trim();
+    // The movie from the textbox is then added to our array
+    topics.push(gif);
 
+    // calling renderButtons which handles the processing of our movie array
+    renderButtons();
+});
+
+renderButtons();
 
 // File through the topics array and display buttons on the window
 
