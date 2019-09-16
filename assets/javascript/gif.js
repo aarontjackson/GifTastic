9907c3d0710d12bd6be1551cf29cc78676b86f44
Aaron Jackson <aarontjackson@outlook.com>
2019-09-15 20:38:46 -0400

    images render on new button creation

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,88 +1,84 @@
-// Create an array of strings
-let topics = ["Orlando", "Florida", "Paris", "France", "Maryland"];
+$(document).ready(function () {
+    console.log("Ready to go!");
 
-console.log("It's working");
+    // Create an array of strings
+    let topics = ["Apple", "Orange", "Grape", "Banana", "Dragonfruit"];
+    console.log("It's working");
 
-// GIPHY URL
-// function displayGif() {
+    // GIPHY URL
+    //javascript, jQuery GIPHY URL, which will include a user input search & 10 gif limit
+    $("button").on("click", function() {
 
-//     let gif = $(this).attr("data-name");
-//     //javascript, jQuery GIPHY URL, which will include a user input search & 10 gif limit
-$("#gifBtn").on("click", function () {
-    var gifImg = $(this).attr("data-name");
-    var queryURL = `"https://api.giphy.com/v1/gifs/search?&api_key=nf5xbhF78wvJVnNt6j0vQ9XrNgNlkPL9&q=${gifImg}&limit=3"`;
+        var gifImg = $(this).attr("data-name");
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log("success got data", response);
+        // var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifImg + "&api_key=" + apiKey + "&limit=5";
 
-        let results = response.data;
+        $.get("http://api.giphy.com/v1/gifs/search?q=" + gifImg + "&api_key=nf5xbhF78wvJVnNt6j0vQ9XrNgNlkPL9&limit=5")
+            .then(function (response) {
+            console.log("data is displaying");
 
-        for (let i = 0; i < results.length; i++) {
-            console.log("Pictures are loading");
-            var imagesDiv = $("<div>");
+            var results = response.data;
+        
+            for (let i = 0; i < results.length; i++) {
+                console.log(results.length);
 
-            let placeImg = $("<img>");
-            placeImg.attr("src", results[i].images.fixed_height.url);
+                renderButtons();
 
-            imagesDiv.prepend(placeImg);
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
 
-        }
-    });
-});
+                    var gifDiv = $("<div>");
 
-// Function to render topics array items
-function renderButtons() {
+                    var ratingVal = $("<p>").text("Rating: " + results[i].rating);
 
-    // Removes buttons that do not display topics array items
-    $("#gifBtn").empty();
+                    var foodImage = $("<img>");
 
-    for (let i = 0; i < topics.length; i++) {
-        console.log("looping was a success");
+                    foodImage.attr("src", results[i].images.fixed_height_still.url);
 
-        let btn = $("<button>");
-        btn.attr({
-            "class": 'gif',
-            "data-name": topics[i]
+                    gifDiv.append(ratingVal);
+                    gifDiv.append(foodImage);
+                    $("#gif-box").prepend(gifDiv);
+                }
+            }
         });
-        btn.text(topics[i]);
-        $("#gifBtn").append(btn);
+    });
+
+
+    // Function to render topics array items
+    function renderButtons() {
+
+        // Removes buttons that do not display topics array items
+        $("#gifBtn").empty();
+
+        for (let i = 0; i < topics.length; i++) {
+            console.log("button was a success");
+
+            var btn = $("<button>");
+            btn.addClass("gif-btn");
+            btn.attr("data-name", topics[i]);
+            btn.text(topics[i]);
+            $("#gifBtn").append(btn);
+        }
+
     }
+    // Take the topics array, and create an element to hold new buttons
+    $("#addGif").on("click", function (event) {
 
-}
-// Take the topics array, and create an element to hold new buttons
-$("#addGif").on("click", function (event) {
+        // event.preventDefault() prevents the form from trying to submit itself.
+        event.preventDefault();
 
-    // event.preventDefault() prevents the form from trying to submit itself.
-    event.preventDefault();
+        // This line will grab the text from the input box
+        var gifVal = $("#gifInput").val().trim();
 
-    // This line will grab the text from the input box
-    let gifName = $("#gifInput").val().trim();
+        // Items from the topics array will be placed on the window as buttons
+        topics.push(gifVal);
 
-    // Items from the topics array will be placed on the window as buttons
-    topics.push(gifName);
-    console.log(topics);
+        // calling renderButtons which handles the processing of our topics array
+        renderButtons();
+    });
 
-    // calling renderButtons which handles the processing of our topics array
+    // $(document).on("click", ".gif-btn", displayGif);
+
     renderButtons();
+  
+
 });
-
-// $(document).on("click", ".gif", displayGif);
-
-renderButtons();
-
-// File through the topics array and display buttons on the window
-
-// Click on the buttons to display 10 Gif images w/o movement
-
-// Click on the Gif images to cause movement
-
-// Display Gif rating above the images
-
-// User input takes value and adds to topic array
-
-// Function that takes in topic array 
-
-// Function remakes buttons on page with User input
